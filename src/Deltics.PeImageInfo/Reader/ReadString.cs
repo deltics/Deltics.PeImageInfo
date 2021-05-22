0194355f7081d43646d3430334031cf5eda82faa
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-05-22 14:53:19 +1200

    refactor: ReadStringU moved to PeResourceInfo

--- src/Deltics.PeImageInfo/Reader/ReadString.cs
+++ src/Deltics.PeImageInfo/Reader/ReadString.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Security;
+using System.Runtime.Serialization;
 using System.Text;
 
 
@@ -9,32 +11,6 @@ namespace Deltics.PeImageInfo.Reader
 {
     public partial class PeReader
     {
-        // Use this to read a string from the unicode string table within the resource
-        //  data section.  Strings in this area are formatted with a leading length
-        //  indicator.  After reading the string from the specified location, the
-        //  reader is restored to the position it was at before the method was called.
-        public string ReadStringU(ulong pos)
-        {
-            var result = "";
-            var org    = GetPosition();
-            try
-            {
-                SetPosition(pos);
-
-                var len   = ReadUInt16();
-                var bytes = ReadBytes(len * 2);
-
-                result = Encoding.Unicode.GetString(bytes);
-            }
-            finally
-            {
-                SetPosition(org);
-            }
-
-            return result;
-        }
-
-
         // Use this method when you do NOT know the length of the string to be read
         //  but do know that the string is null-terminated.
         public string ReadStringZ()
